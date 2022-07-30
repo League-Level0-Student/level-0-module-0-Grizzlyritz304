246ff1ea01190441258592ad6b64aaa53b7f32b0
Your Name <you@example.com>
2022-07-30 14:30:22 -0700

    just finished scary maze. BTW hi future me

--- src/_04_amazing_games/_2_dragon_fight/DragonFight.java
+++ src/_04_amazing_games/_2_dragon_fight/DragonFight.java
@@ -14,19 +14,19 @@ public class DragonFight {
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 
 		// 1. Create some variables to hold health levels
-		
+		int playerHealth=100;
 			// playerHealth to store your health - set it equal to 100
-	
+	    int dragonHealth=1000;
 			// dragonHealth to store the dragon's health - set it equal to 100
 
 		// 2. Create some variables to hold the attack strengths. These will be given different values later. 
 		
 			// playerAttack to store the damage the player's attack will do - set it equal
 			// to 0 for now.
-	
+	    int playerAttack=0;
 			// dragonAttack to store the damage the dragon's attack will do - set it equal
 			// to 0 for now.
-
+        int dragonAttack=0;
 		
 		//  This while statement will cause the game attack code to repeat
 		while (true) {
